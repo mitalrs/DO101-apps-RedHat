57a780750d7ca575f7da80e986b07e51a2c3ecc6
mitalrs <mitalsapkale@gmail.com>
2022-04-23 13:07:46 +0530

    update-weather

--- weather/app.js
+++ weather/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+//var indexRouter = require('./routes/index');
 
 var app = express();
 
