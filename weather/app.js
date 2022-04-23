2a927fe2b7db5a8f8bfbd0b12ed81bb31c23d3bf
mitalrs <mitalsapkale@gmail.com>
2022-04-24 02:47:08 +0530

    index router fixed

--- weather/app.js
+++ weather/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-app.use('/', indexRouter);
-//var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
